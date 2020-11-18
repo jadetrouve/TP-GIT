16c24964b2fb3b43a0d26c290b910a372e2c18df
hugoB00544 <hugo.blanchard00544@gmail.com>
2020-11-18 17:47:30 +0100

    force_vert

--- Untitled-1.java
+++ Untitled-1.java
@@ -5,21 +5,17 @@ import java.util.*;
 */
 public class LabClass
 {
- private String instructeur3;
+ 
  private String room;
- private String timeAndDay;
- private List<Student> students;
- private int capacity;
+ 
 
  /**
  * Create a LabClass with a maximum number of enrolments. All other details are set to default values.
  */
  public LabClass(int maxNumberOfStudents)
  {
- instructeur3 = "ce que vous voulez";
+ 
  room = " mettez ce que vous voulez ";
- timeAndDay = " mettez ce que vous voulez ";
- students = new ArrayList<Student>();
- capacity = maxNumberOfStudents;
+ 
  }
 }
