6c5ad9c5ae4469cbcdaa8532b1ee30028f41aea6
MatthewBrowne <brownematthew93@yahoo.fr>
2020-11-18 15:28:01 +0100

    Matthew changement 1

---
+++ Untitled-1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+/**
+* The LabClass class represents an enrolment list for one lab class. It stores the time, room and participants
+* of the lab, as well as the instructor's name.
+*/
+public class LabClass
+{
+ private String instructeur;
+ private String room;
+ private String timeAndDay;
+ private List<Student> students;
+ private int capacity;
+
+ /**
+ * Create a LabClass with a maximum number of enrolments. All other details are set to default values.
+ */
+ public LabClass(int maxNumberOfStudents)
+ {
+ instructeur = "ce que vous voulez";
+ room = " mettez ce que vous voulez ";
+ timeAndDay = " mettez ce que vous voulez ";
+ students = new ArrayList<Student>();
+ capacity = maxNumberOfStudents;
+ }
+}
